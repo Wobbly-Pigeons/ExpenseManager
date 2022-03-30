83bb952ee0ac079198717c488eca297e0ca3f4dd
awilkester <awilkes2589@gmail.com>
2022-03-30 15:42:31 +0200

    added policy models

---
+++ src/main/java/wobbly/pigeons/expensemanager/models/DepartmentPolicy.java
@@ -0,0 +1,14 @@
+package wobbly.pigeons.expensemanager.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+public class DepartmentPolicy extends Policy{
+}
