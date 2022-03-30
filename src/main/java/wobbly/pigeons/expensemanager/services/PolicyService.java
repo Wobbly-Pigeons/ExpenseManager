618c46334ef1db993330f585869579e33ca99052
awilkester <awilkes2589@gmail.com>
2022-03-30 16:18:04 +0200

    added repository and service classes. I expect these to change, maybe not significantly, but at least a little

---
+++ src/main/java/wobbly/pigeons/expensemanager/services/PolicyService.java
@@ -0,0 +1,40 @@
+package wobbly.pigeons.expensemanager.services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import wobbly.pigeons.expensemanager.models.DepartmentPolicy;
+import wobbly.pigeons.expensemanager.models.IndividualPolicy;
+import wobbly.pigeons.expensemanager.repository.DepartmentPolicyRepository;
+import wobbly.pigeons.expensemanager.repository.IndividualPolicyRepository;
+
+@Service
+@AllArgsConstructor
+public class PolicyService {
+    private final DepartmentPolicyRepository departmentPolicyRepository;
+    private final IndividualPolicyRepository individualPolicyRepository;
+
+    //CRUD
+    public IndividualPolicy findIPolicy(Long id) {
+        return individualPolicyRepository.getById(id);
+    }
+
+    public DepartmentPolicy findDPolicy(Long id) {
+        return departmentPolicyRepository.getById(id);
+    }
+
+    public IndividualPolicy createIPolicy(IndividualPolicy newIPolicy) {
+        return individualPolicyRepository.save(newIPolicy);
+    }
+
+    public void deleteIPolicy (Long id) {
+        individualPolicyRepository.deleteById(id);
+    }
+
+    public IndividualPolicy updateIPolicy(IndividualPolicy updatedPolicy, Long id) {
+        IndividualPolicy oldPolicy = updatedPolicy;
+        return individualPolicyRepository.save(oldPolicy);
+    }
+
+
+
+}
