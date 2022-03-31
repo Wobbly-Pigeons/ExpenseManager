fd95e34bbeee94393ddb496348c082bc8da5e4b5
Miguel Silva <49847120+MiguelSilva23@users.noreply.github.com>
2022-03-31 13:13:50 +0200

    Department,Employees,Managers Controller

---
+++ src/main/java/wobbly/pigeons/expensemanager/controllers/DepartmentController.java
@@ -0,0 +1,40 @@
+package wobbly.pigeons.expensemanager.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import wobbly.pigeons.expensemanager.models.Department;
+import wobbly.pigeons.expensemanager.services.DepartmentService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/api/v1/Departments")
+public class DepartmentController {
+
+    private DepartmentService departmentService;
+
+    @GetMapping
+    public List<Department> getDepartmentsList() {
+        return departmentService.getDepartmentsList();
+    }
+
+    @PostMapping
+    public Department addDepartment(Department department) {
+        return departmentService.addDepartment(department);
+    }
+
+    @GetMapping("/{id}")
+    public Department getDepartment(@PathVariable Long id) {
+        return departmentService.getDepartment(id);
+    }
+
+    @PutMapping("/{id}")
+    public Department updateDepartment(@PathVariable Long id, @RequestParam Department department) {
+        return departmentService.updateDepartment(id, department);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteDepartment(@PathVariable Long id) {
+        departmentService.deleteDepartment(id);
+    }
+}
