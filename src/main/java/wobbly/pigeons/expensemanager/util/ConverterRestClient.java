1ddef589eaaf57b3cb23c167b77a65a64787663d
abickmore <bickmorebusiness@yahoo.com>
2022-04-07 14:21:53 +0200

    #39 re added currency converter and cleaned up mess

---
+++ src/main/java/wobbly/pigeons/expensemanager/util/ConverterRestClient.java
@@ -0,0 +1,48 @@
+package wobbly.pigeons.expensemanager.util;
+
+
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
+
+
+public class ConverterRestClient {
+
+    private final WebClient webClient;
+    private String myAPIkey = "9d2cc16424081f3851cfd3ae";
+
+    public ConverterRestClient(){
+        webClient = WebClient.builder()
+                .baseUrl("https://v6.exchangerate-api.com/v6/" + myAPIkey)
+                .build();
+    }
+
+
+    public Double getConversionAmount (String baseCode, String targetCode, double amount){
+
+        return (Double) webClient
+                .get()
+                // .uri("/pair/{base_code}/target_code/{amount}")
+                .uri (uriBuilder -> uriBuilder
+                        .path ("/pair")
+                        .path("/" + baseCode)
+                        .path("/" + targetCode)
+                        .path("/" + amount)
+                        .build()
+                )
+                .retrieve()
+                .bodyToMono(Map.class)
+                .map(map -> map.get("conversion_result"))
+                .block();
+    }
+//LEARNING NOTES
+    //        Function<Map, Object> function = new Function<>() {
+    //
+    //            @Override
+    //            public Object apply(Map map) {
+    //                return map.get("conversion_result");
+    //            }
+    //        };
+
+
+}
